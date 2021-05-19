d393a8e7c5a63b51fbd130f22f803dd0417bb0f0
interactive-game-maps <>
2021-05-19 16:43:11 +0200

    structure

---
+++ map_layers.js
@@ -0,0 +1,29 @@
+var map = L.map('map', {
+    crs: L.CRS.Simple,
+    // minZoom: 0,
+    maxZoom: 10,
+    zoom: 3
+});
+
+// Use tiled maps if possible, allows better zooming
+// Make sure tiling scheme is growing downwards!
+// https://github.com/Leaflet/Leaflet/issues/4333#issuecomment-199753161
+// https://github.com/commenthol/gdal2tiles-leaflet
+// ./gdal2tiles.py -l -p raster -w none -z 2-5 full_map.jpg map_tiles
+var tiled_map = new L.tileLayer('map_tiles/{z}/{x}/{y}.png', {
+    minNativeZoom: 2,
+    maxNativeZoom: 5,
+    attribution: '<a href="https://steamcommunity.com/sharedfiles/filedetails/?id=280577526">Map and images from EnigmaticRunner</a>',
+    noWrap: true,
+    detectRetina: true
+});
+
+var baseMaps = {
+    "Ingame map": tiled_map
+};
+
+// Make one base layer visible by default
+tiled_map.addTo(map);
+
+// Add siedbar to map
+var sidebar = L.control.sidebar('sidebar').addTo(map);
--- map_logic.js
+++ map_logic.js
@@ -1,32 +1,3 @@
-var map = L.map('map', {
-    crs: L.CRS.Simple,
-    // minZoom: 0,
-    maxZoom: 10,
-    zoom: 3
-});
-
-{ // Defining base maps
-    // Use tiled maps if possible, allows better zooming
-    // Make sure tiling scheme is growing downwards!
-    // https://github.com/Leaflet/Leaflet/issues/4333#issuecomment-199753161
-    // https://github.com/commenthol/gdal2tiles-leaflet
-    // ./gdal2tiles.py -l -p raster -w none -z 2-5 full_map.jpg map_tiles
-    var tiled_map = new L.tileLayer('map_tiles/{z}/{x}/{y}.png', {
-        minNativeZoom: 2,
-        maxNativeZoom: 5,
-        attribution: '<a href="https://steamcommunity.com/sharedfiles/filedetails/?id=280577526">Map and images from EnigmaticRunner</a>',
-        noWrap: true,
-        detectRetina: true
-    });
-
-    var baseMaps = {
-        "Ingame map": tiled_map
-    };
-
-    // Make one base layer visible by default
-    tiled_map.addTo(map);
-}
-
 { // Helper functions
     function add_checkbox_for_marker(feature, marker, list, cluster) {
         // Add checkbox for marker
@@ -101,17 +72,3 @@ var map = L.map('map', {
 
 // Add user selection to map
 L.control.layers(baseMaps, overlayMaps).addTo(map);
-
-// Add siedbar to map
-var sidebar = L.control.sidebar('sidebar').addTo(map);
-
-{ // Coordinate Finder, disable after getting all positions
-    // https://www.techtrail.net/creating-an-interactive-map-with-leaflet-js/
-    // var marker = L.marker([0, 0], {
-    //     draggable: true,
-    // }).addTo(map);
-    // marker.bindPopup('LatLng Marker').openPopup();
-    // marker.on('dragend', function (e) {
-    //     marker.getPopup().setContent('<span style="white-space: pre">' + JSON.stringify(marker.toGeoJSON(), null, 4) + '</span>').openOn(map);
-    // });
-}
