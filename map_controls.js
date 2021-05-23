2b8505407fd73b9535aad8b3343fe4bf895542f6
interactive-game-maps <>
2021-05-23 20:57:39 +0200

    linkable marker

--- map_controls.js
+++ map_controls.js
@@ -1,14 +1,35 @@
-{ // Defining overlay maps - markers
-    var overlayMaps = {
-        "Treasure Bags": treasure_bag_cluster
-    };
+// Defining overlay maps - markers
+var overlayMaps = {
+    "Treasure Bags": treasure_bags_group
+};
 
-    // Make overlay layer visible by default
-    map.addLayer(treasure_bag_cluster);
+// Make overlay layer visible by default
+map.addLayer(treasure_bags_group);
 
-    // Center view over map
-    map.fitBounds([[0, 0], [-256, 256]]);
+// Center view over map
+map.fitBounds([[0, 0], [-256, 256]]);
 
-    // Add user selection to map
-    L.control.layers(baseMaps, overlayMaps).addTo(map);
+// Add user selection to map
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+// Search in url for marker and locate them
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+if (urlParams.has('list')) {
+    const list = urlParams.get('list');
+    if (marker.get(list).has('group')) {
+        // make group visible
+        map.addLayer(marker.get(list).get('group'));
+    }
+    if (!urlParams.has('id')) {
+        // if no id open sidebar
+        sidebar.open(list);
+    }
+    else {
+        const id = urlParams.get('id');
+        if (marker.has(list) && marker.get(list).has(id)) {
+            // center and zoom id
+            map.fitBounds(L.latLngBounds([marker.get(list).get(id).getLatLng()]));
+        }
+    }
 }
--- marker_logic/treasure_bags.js
+++ marker_logic/treasure_bags.js
@@ -3,9 +3,12 @@ var treasure_bags_list = document.createElement('ul');
 treasure_bags_list.className = 'collectibles_list';
 
 // Create marekr group
-var treasure_bag_cluster = L.markerClusterGroup({
+var treasure_bags_group = L.markerClusterGroup({
     maxClusterRadius: 40
 });
+
+// save all marker in a map so we can access them later
+var treasure_bags_map = new Map();
 
 // Add all markers and attach popups with information
 L.geoJSON(treasure_bags, {
@@ -21,7 +24,9 @@ L.geoJSON(treasure_bags, {
             })
         });
 
-        if (!add_checkbox_for_marker(feature, marker, treasure_bags_list, "treasure_bags", treasure_bag_cluster)) {
+        // Add marker to lists
+        treasure_bags_map.set(feature.properties.number.toString(), marker);
+        if (!add_checkbox_for_marker(feature, marker, treasure_bags_list, "treasure_bags", treasure_bags_group)) {
             return null;
         }
         return marker;
@@ -29,8 +34,17 @@ L.geoJSON(treasure_bags, {
     onEachFeature: function (feature, layer) {
         // popup with simple image and description
         layer.bindPopup("<a href='https://steamuserimages-a.akamaihd.net/ugc/" + feature.properties.id + "/'><img src='https://steamuserimages-a.akamaihd.net/ugc/" + feature.properties.id + "/' width='500/' /></a>" + feature.properties.description, { maxWidth: 500 });
+
+        // rewrite url for easy copy pasta
+        layer.on('popupopen', (event) => {
+            history.replaceState({}, "", "index.html?list=" + "treasure_bags" + "&id=" + feature.properties.number);
+        });
     }
-}).addTo(treasure_bag_cluster);
+}).addTo(treasure_bags_group);
+treasure_bags_map.set("group", treasure_bags_group);
+
+// save local list in global list of lists
+marker.set("treasure_bags", treasure_bags_map);
 
 // Add list to sidebar
 sidebar.addPanel({
