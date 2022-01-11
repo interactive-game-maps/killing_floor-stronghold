8cdb6d57bc07d0021119c9be5f5c368a7fde9dd7
interactive-game-maps <>
2022-01-11 14:43:53 +0100

    Backport latest improvements

--- marker_logic/treasure_bags.js
+++ marker_logic/treasure_bags.js
@@ -1,18 +1,9 @@
-// Create list
-var treasure_bags_list = document.createElement('ul');
-treasure_bags_list.className = 'collectibles_list';
-
-// Add list to sidebar
 var treasure_bags_group_name = 'Treasure Bags';
-sidebar.addPanel({
-    id: 'treasure_bags',
-    tab: '💰',
-    title: treasure_bags_group_name,
-    pane: '<p></p>' // placeholder to get a proper pane
-});
-document.getElementById('treasure_bags').appendChild(treasure_bags_list);
+var treasure_bags_group_id = 'treasure_bags';
+var treasure_bags_create_checkbox = true;
 
-// Create marekr group
+var treasure_bags_list = createSidebarTab(treasure_bags_group_id, treasure_bags_group_name, '💰');
+
 var treasure_bags_group = L.markerClusterGroup({
     maxClusterRadius: 40
 });
@@ -33,13 +24,24 @@ L.geoJSON(treasure_bags, {
         });
     },
     onEachFeature: (feature, layer) => {
-        onEachFeature(feature, layer, {
+        addPopup(feature, layer, {
             layer_group: treasure_bags_group,
             list: treasure_bags_list,
-            list_name: "treasure_bags",
-            create_checkbox: true
+            list_id: treasure_bags_group_id,
+            create_checkbox: treasure_bags_create_checkbox
+        });
+        saveMarker(feature, layer, {
+            list_id: treasure_bags_group_id
         });
     }
 }).addTo(treasure_bags_group);
-marker.get('treasure_bags').set('group', treasure_bags_group);
-marker.get('treasure_bags').set('name', treasure_bags_group_name);
+marker.get(treasure_bags_group_id).set('group', treasure_bags_group);
+marker.get(treasure_bags_group_id).set('name', treasure_bags_group_name);
+
+if (treasure_bags_create_checkbox) {
+    setColumnCount(marker.get(treasure_bags_group_id), treasure_bags_list);
+}
+
+// Add as a default layer
+// This needs the display name because the layer control don't report ids
+default_layers.push(treasure_bags_group_name);
