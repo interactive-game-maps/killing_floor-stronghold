f9a5f885e7d599ea040efca8502a51b5b2447c79
interactive-game-maps <>
2022-01-20 00:04:18 +0100

    Much better

--- marker_logic/treasure_bags.js
+++ marker_logic/treasure_bags.js
@@ -1,37 +1,15 @@
-var treasure_bags_group_name = 'Treasure Bags';
-var treasure_bags_group_id = 'treasure_bags';
-var treasure_bags_create_checkbox = true;
-
-var treasure_bags_list = createSidebarTab(treasure_bags_group_id, treasure_bags_group_name, '💰');
-
-var treasure_bags_group = L.featureGroup.subGroup(marker_cluster);
-
-L.geoJSON(treasure_bags, {
-    pointToLayer: (feature, latlng) => {
+var treasure_bags_layer = new InteractiveLayer('treasure_bags', treasure_bags, {
+    name: "Treasure Bags",
+    create_checkbox: true,
+    create_feature_popup: true,
+    is_default: true,
+    sidebar_icon_html: '💰',
+    pointToLayer: function (feature, latlng) {
         return L.marker(latlng, {
             icon: getCustomIcon('💰'),
             riseOnHover: true
         });
-    },
-    onEachFeature: (feature, layer) => {
-        addPopup(feature, layer, {
-            layer_group: treasure_bags_group,
-            list: treasure_bags_list,
-            list_id: treasure_bags_group_id,
-            create_checkbox: treasure_bags_create_checkbox
-        });
-        saveMarker(feature, layer, {
-            list_id: treasure_bags_group_id
-        });
     }
-}).addTo(treasure_bags_group);
-marker.get(treasure_bags_group_id).set('group', treasure_bags_group);
-marker.get(treasure_bags_group_id).set('name', treasure_bags_group_name);
+});
 
-if (treasure_bags_create_checkbox) {
-    setColumnCount(marker.get(treasure_bags_group_id), treasure_bags_list);
-}
-
-// Add as a default layer
-// This needs the display name because the layer control don't report ids
-default_layers.push(treasure_bags_group_name);
+interactive_layers.set(treasure_bags_layer.id, treasure_bags_layer);
